dd62f616940d25f0767d75b9714f8fb3366fbd1a
jinjor <jinjorweb@gmail.com>
2019-07-31 12:16:49 +0900

    refactor example

--- test/example.ts
+++ test/example.ts
@@ -29,7 +29,6 @@ import {
 import * as util from "util";
 import { readFileSync } from "fs";
 import * as assert from "assert";
-import { json_sample1k } from "./json1k";
 
 describe("Examples", function() {
   this.timeout(50000);
@@ -99,58 +98,65 @@ describe("Examples", function() {
     );
     const json = seq($2, _, val, _, end);
 
-    compareJSON(__dirname + "/../package.json");
-    compareJSON(__dirname + "/../package-lock.json");
-    compareJSON("1k sample", json_sample1k);
+    compareTime(__dirname + "/../package.json");
+    compareTime(__dirname + "/../package-lock.json");
+    compareTime(__dirname + "/1k.json");
 
-    function compareJSON(file: string, source?: string) {
-      const count = 1000;
-      console.log(`comparing ${file} ...`);
-      source = source || readFileSync(file, "utf8");
-      let ast;
-      let ast2;
-      const s1 = Date.now();
-      for (let i = 0; i < count; i++) {
-        ast = run(json, source);
-      }
-      const e1 = Date.now();
-      const s2 = Date.now();
-      for (let i = 0; i < count; i++) {
-        ast2 = JSON.parse(source);
-      }
-      const e2 = Date.now();
-      console.log(`  typed-parser: ${(e1 - s1) / count}ms`);
-      console.log(`  native parser: ${(e2 - s2) / count}ms`);
-      assert.deepEqual(ast2, ast);
-      console.log();
-    }
-    function compareJSON2(file: string, source?: string) {
-      console.log(`comparing ${file} ...`);
-      source = source || readFileSync(file, "utf8");
-      let ast;
-      let ast2;
-      const s1 = Date.now();
-      let op1 = 0;
-      while (Date.now() - s1 < 1000) {
-        ast = run(json, source);
-        op1++;
-      }
-      const s2 = Date.now();
-      let op2 = 0;
-      while (Date.now() - s2 < 1000) {
-        ast2 = JSON.parse(source);
-        op2++;
-      }
-      console.log(`  typed-parser: ${op1}ops/s`);
-      console.log(`  native parser: ${op2}ops/s`);
-      assert.deepEqual(ast2, ast);
-      console.log();
-    }
     try {
       const source = readFileSync(__dirname + "/broken.json", "utf8");
       run(json, source);
     } catch (e) {
       console.log((e as ParseError).explain());
+      console.log();
+    }
+
+    function measureTime(count: number, f: () => void): number {
+      const start = Date.now();
+      for (let i = 0; i < count; i++) {
+        f();
+      }
+      return Date.now() - start;
+    }
+    function measureOps(f: () => void): number {
+      const start = Date.now();
+      let ops = 0;
+      while (Date.now() - start < 1000) {
+        f();
+        ops++;
+      }
+      return ops;
+    }
+    function compareTime(file: string) {
+      const count = 1000;
+      console.log(`comparing ${file} ...`);
+      const source = readFileSync(file, "utf8");
+      let value1;
+      let value2;
+      const time1 = measureTime(count, () => {
+        value1 = run(json, source);
+      });
+      const time2 = measureTime(count, () => {
+        value2 = JSON.parse(source);
+      });
+      console.log(`  typed-parser: ${time1 / count}ms`);
+      console.log(`  native parser: ${time2 / count}ms`);
+      assert.deepEqual(value2, value1);
+      console.log();
+    }
+    function compareOps(file: string) {
+      console.log(`comparing ${file} ...`);
+      const source = readFileSync(file, "utf8");
+      let value1;
+      let value2;
+      const ops1 = measureOps(() => {
+        value1 = run(json, source);
+      });
+      const ops2 = measureOps(() => {
+        value2 = JSON.parse(source);
+      });
+      console.log(`  typed-parser: ${ops1}ops/s`);
+      console.log(`  native parser: ${ops2}ops/s`);
+      assert.deepEqual(value2, value1);
       console.log();
     }
   });
